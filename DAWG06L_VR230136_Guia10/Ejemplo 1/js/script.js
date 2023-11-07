9a4b09834eb5f9f35e91145649f89cdf5d5a94a8
Carislost <135508493+Carislost@users.noreply.github.com>
2023-11-07 16:12:04 -0600

    Desarrollo Guia 10

---
+++ DAWG06L_VR230136_Guia10/Ejemplo 1/js/script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("ul.parent > li").hover(
+    function () {
+      $(this).find("ul.child").show(400);
+    },
+    function () {
+      $(this).find("ul.child").hide(200);
+    }
+  );
+});
